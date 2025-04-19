375eadcfd2b4a025dbf450aff8f2e33e3d60922e
Lucho <lucianomalica@gmail.com>
2025-04-18 22:51:11 -0300

    Reinicio del backend y cambio de lenguaje a C#

---
+++ Backend/Models/plant.cs
@@ -0,0 +1,9 @@
+namespace SmartLeaf.Backend.Models
+{
+    public class Plant
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Especie { get; set; }
+    }
+}
