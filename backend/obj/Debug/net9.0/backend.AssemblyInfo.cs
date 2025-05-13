62b86023df6d74d3555ed67406276632bdd6312a
MarianhoRuiz <marianoruizcoronel@gmail.com>
2025-05-13 11:28:37 -0300

    Añadir cambio automatico de Apikeys de PlantID

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39c432dd1c33f844b953ab814b02eb61826b9ad4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39fdc3832169a5ef9142f4b83c2079fd34894133")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
