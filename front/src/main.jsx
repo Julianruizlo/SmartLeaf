d674961d8df3229a0a4fb86674a7757a49549667
Facu-hob <facundopuentesgg@gmail.com>
2025-05-13 09:50:12 -0300

    limpieza de rama

--- front/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './router/App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <App />
-)
--- front/src/utils/wateringScheduler.jsx
+++
@@ -1,5 +0,0 @@
-export function getNextWateringDate(plant) {
-    const lastWatered = new Date(plant.lastWatered);
-    return new Date(lastWatered.setDate(lastWatered.getDate() + plant.wateringFrequency));
-  }
-  
