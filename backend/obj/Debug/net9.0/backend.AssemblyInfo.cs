eb44d23dc8816608729aff6f438549b64fe827ff
Facu-hob <facundopuentesgg@gmail.com>
2025-05-14 16:26:57 -0300

    .

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7c061b5c40710bf4d7829f48ad6b0f8ae0225a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01b68a74e2ec17fd7ab87675b115555959f62890")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
